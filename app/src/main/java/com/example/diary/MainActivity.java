529bc8129c46627d245b56c32a4827f7845cc52e
zhuangShiqing <84309103+zhuangShiqing@users.noreply.github.com>
2022-01-05 14:12:26 +0800

    108360107

--- app/src/main/java/com/example/diary/MainActivity.java
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button diary_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        diary_add=(Button)findViewById(R.id.add_diary);
+        Button toadd_diary=(Button) findViewById(R.id.add_diary);
+        toadd_diary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(MainActivity.this,diary_add.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/diary/diary_add.java
+++ app/src/main/java/com/example/diary/diary_add.java
@@ -2,13 +2,47 @@ package com.example.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class diary_add extends AppCompatActivity {
-
+    private TextView result_date; //顯示選擇日期
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_add);
+
+        Button sel_date=(Button) findViewById(R.id.sel_date);
+        result_date=(TextView) findViewById(R.id.result_date);
+        sel_date.setOnClickListener(sel_dateOnClick);
     }
+    private Button.OnClickListener sel_dateOnClick=new Button.OnClickListener(){
+        public void onClick(View v){
+            Calendar now=Calendar.getInstance();
+            DatePickerDialog date=new DatePickerDialog(diary_add.this,
+                    date_set,
+                    now.get(Calendar.YEAR),
+                    now.get(Calendar.MONTH),
+                    now.get(Calendar.DAY_OF_MONTH));
+            date.setTitle("選擇日期");
+            date.setMessage("請選擇您日記記錄的時間");
+            date.setCancelable(false);
+            date.show();
+        }
+    };
+    private DatePickerDialog.OnDateSetListener date_set=
+            new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int monthofyear, int dayofmonth) {
+            result_date.setText("日記時間"+Integer.toString(year)+"年"+
+                                Integer.toString(monthofyear+1)+"月"+
+                                Integer.toString(dayofmonth)+"日");
+        }
+    };
 }
