22e7b1927b95c96850df01d11c0c616fbdc39639
zhuangShiqing <84309103+zhuangShiqing@users.noreply.github.com>
2022-01-06 11:19:43 +0800

    108360107

--- app/src/main/java/com/example/diary/MainActivity.java
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -4,28 +4,72 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.Serializable;
 
+public class MainActivity extends AppCompatActivity {
+    //定義生命週期Tag標籤
+    private static final String LOG_TAG="ActivityLifeCycle";
     private Button diary_add;
+    //宣告前往新增日記頁面
+    public Intent intent_toadd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG,"MainActivity.onCreate");
         setContentView(R.layout.activity_main);
         diary_add=(Button)findViewById(R.id.add_diary);
         Button toadd_diary=(Button) findViewById(R.id.add_diary);
         toadd_diary.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent();
-                intent.setClass(MainActivity.this,diary_add.class);
-                startActivity(intent);
+                intent_toadd=new Intent(MainActivity.this,diary_add.class);
+                startActivity(intent_toadd);
+
             }
         });
 
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG,"MainActivity.onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG,"MainActivity.onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG,"MainActivity.onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG,"MainActivity.onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG,"MainActivity.onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG,"MainActivity.onRestart");
+    }
 }
+
--- app/src/main/java/com/example/diary/diary_add.java
+++ app/src/main/java/com/example/diary/diary_add.java
@@ -3,10 +3,14 @@ package com.example.diary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,52 +24,48 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class diary_add extends AppCompatActivity {
-
-    public ArrayAdapter<String> adapter;
-    private ArrayList<String> diary_items=new ArrayList<>();
+    private static final String LOG_TAG_d="diary_addLifeCycle";
+    //宣告資料庫名稱及資料表名稱
+    static final String db_name="mydiary";//資料庫名稱
+    static final String tb_name="mydiary_list"; //資料表名稱
+    SQLiteDatabase mydiary; //資料庫物件
     //連結介面元件參數宣告
-    private TextView result_date; //顯示選擇日期
-    private EditText diary_content,diary_topic;
-    private SQLiteDatabase mydiary;
-    private ListView listView;
-    private Button btn_save;
+    public TextView result_date; //顯示選擇日期
+    public EditText diary_content,diary_topic; //日記的主題以及內文
+    private ListView listView; //日記列表
+    private Button btn_save,sel_date;  //儲存日記按鈕及設定日期按鈕
+    public Intent intent_back; //建立回我的日記的頁面
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG_d,"MainActivity.onCreate");
+        //設定連接的介面佈局檔
         setContentView(R.layout.activity_diary_add);
-
-        Button sel_date=(Button) findViewById(R.id.sel_date);
-        result_date=(TextView) findViewById(R.id.result_date);
-        sel_date.setOnClickListener(sel_dateOnClick);
-
         //連結介面元件
+        result_date=(TextView) findViewById(R.id.result_date);
         diary_content=(EditText) findViewById(R.id.diary_content);
         diary_topic=(EditText) findViewById(R.id.diary_topic);
         listView=(ListView)findViewById(R.id.diary_list);
         btn_save=(Button)findViewById(R.id.add_button);
-        //宣告Adapter
-        adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,diary_items);
-        listView.setAdapter(adapter);
-        //取得資料庫實體
-        mydiary=new MyDiary_SQL(this).getWritableDatabase();
-
-        //儲存按鈕
+        sel_date=(Button) findViewById(R.id.sel_date);
+        //選取日期
+        sel_date.setOnClickListener(sel_dateOnClick);
+        //開啟或建立資料庫
+        mydiary=openOrCreateDatabase(db_name, Context.MODE_PRIVATE,null);
+        String createTable="CREATE TABLE myTable(topic text PRIMARY KEY ,date text NOT NULL,content text NOT NULL)";
+        mydiary.execSQL(createTable);
+        //儲存按鈕監聽
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 //查詢myTable資料表
-
+                //確認是否有填寫欄位，不為空值
                 if(diary_topic.length()<1 || diary_content.length()<1)
                     Toast.makeText(diary_add.this,"請填寫主題及日記內容",Toast.LENGTH_SHORT).show();
                 else {
                     try {
-                        mydiary.execSQL("INSERT INTO myTable(diary_topic,result_diary,diary_content)VALUES(?,?,?)",
-                                new Object[]{diary_topic.getText().toString(),
-                                result_date.getText().toString(),
-                                diary_content.getText().toString()});
-                        Toast.makeText(diary_add.this,"新增日記"+diary_topic.getText().toString()+
-                                "日記日期:"+result_date.getText().toString(),
-                                Toast.LENGTH_SHORT).show();
+                        //呼叫add_diary()方法寫入資料表
+                        add_diary(diary_topic.getText().toString(),result_date.getText().toString(),diary_content.getText().toString());
                         //清空輸入
                         diary_topic.setText("");
                         result_date.setText("日記時間:____年__月__日");
@@ -74,10 +74,26 @@ public class diary_add extends AppCompatActivity {
                         Toast.makeText(diary_add.this, "儲存失敗:"+e.toString(), Toast.LENGTH_LONG).show();
                     }
                 }
+                //回到主畫面-我的日記
+                intent_back=new Intent(diary_add.this,MainActivity.class);
+                startActivity(intent_back);
 
             }
         });
 
+    }
+    private void add_diary(String topic_diary,String date_diary,String content_diary){
+        /*
+        將日記加入至資料表中儲存起來
+        * */
+        //設定三欄位的資料表
+        ContentValues cv=new ContentValues(3);
+        //將資料put進去
+        cv.put("Topic",topic_diary);
+        cv.put("Date",date_diary);
+        cv.put("Content",content_diary);
+        //將資料加入到資料表
+        mydiary.insert(tb_name,null,cv);
     }
     private Button.OnClickListener sel_dateOnClick=new Button.OnClickListener(){
         public void onClick(View v){
@@ -105,8 +121,38 @@ public class diary_add extends AppCompatActivity {
     @Override
     public void onDestroy(){
         super.onDestroy();
+        Log.d(LOG_TAG_d,"MainActivity.onDestroy");
         mydiary.close();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG_d,"MainActivity.onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG_d,"MainActivity.onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG_d,"MainActivity.onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG_d,"MainActivity.onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG_d,"MainActivity.onRestart");
+    }
 }
 
