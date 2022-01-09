7498db463adfde5cbf52c5e335a8aa963d196368
鄭璇 <t108360112@ntut.org.tw>
2022-01-09 15:13:15 +0800

    108360112

--- app/src/main/java/com/example/diary/MainActivity.java
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -2,128 +2,72 @@ package com.example.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
-import android.database.Cursor;
-import android.widget.AdapterView;
 import android.widget.SimpleCursorAdapter;
-import android.database.sqlite.SQLiteDatabase;
-import android.content.Context;
 import android.widget.TextView;
 
-import java.io.Serializable;
-
 public class MainActivity extends AppCompatActivity implements ListView.OnItemClickListener{
-    //定義生命週期Tag標籤
-    private static final String LOG_TAG="ActivityLifeCycle";
-    private Button add_diary,exit_diary;
-    private ListView diary_list;
-    //宣告前往新增日記頁面
-    public Intent intent_toadd,intent_back;
+
+    //宣告物件
+    private SQLiteDatabase db = null;
+    ListView list;
+    Button btn_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(LOG_TAG,"MainActivity.onCreate");
         setContentView(R.layout.activity_main);
 
-        private SQLiteDatabase db = null;
-        add_diary=(Button)findViewById(R.id.add_diary);
-        exit_diary=(Button)findViewById(R.id.exit_button);
-        diary_list = (ListView) findViewById(R.id.diary_list);
+        list = (ListView)findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
 
+        //建立SQLite 資料庫的實體檔案
+        db = openOrCreateDatabase("data.db", Context.MODE_PRIVATE,null);
 
-        db = openOrCreateDatabase("data.db", Context.MODE_PRIVATE, null);
+        //建立資料表的SQL語法
+        String strcreatedb = "CREATE TABLE IF NOT EXISTS " +
+                "mytable (_id INTEGER PRIMARY KEY, topic TEXT, date TEXT, content TEXT) ";
 
-        String strcreatedb = "CREATE TABLE myTable(_id INTEGER PRIMARY KEY,topic text NOT NULL ,date text NOT NULL,content text NOT NULL)";
-
+        //執行SQL語法，建立資料表
         db.execSQL(strcreatedb);
 
-        String str1 = "INSERT INTO myTable (topic,date,content) values " +
-                "('a','20201202','abc')";
-        db.execSQL(str1);
+        //查詢資料
+        Cursor cursor = db.rawQuery("SELECT * FROM mytable",null);
 
-        str1 = "INSERT INTO myTable (topic,date,content) values " +
-                "('b','20201203','bcd')";
-        db.execSQL(str1);
-
-        Cursor cursor = db.rawQuery("SELECT * FROM myTable",null);
-
-        if (cursor != null && cursor.getCount() >=0) {
+        //如果查詢有資料的話，就跟ListView做連結
+        if (cursor != null && cursor.getCount() >=0)
+        {
             SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
                     android.R.layout.simple_list_item_2,
                     cursor,
-                    new String[]{"topic", "date"},
-                    new int[]{android.R.id.text1, android.R.id.text2},
+                    new String[] {"_id","topic"},
+                    new int[] {android.R.id.text1,android.R.id.text2},
                     0);
 
-            diary_list.setAdapter(adapter);
-            db.close();
+            list.setAdapter(adapter);
+            //db.close();
         }
-        diary_list.setOnItemClickListener(this);
-        /*
-        Button toadd_diary=(Button) findViewById(R.id.add_diary);
-        Button back_cover=(Button)findViewById(R.id.exit_button);
-         */
-        //點擊新增日記按鈕
-        add_diary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intent_toadd=new Intent(MainActivity.this,diary_add.class);
-                startActivity(intent_toadd);
-            }
-        });
-        //點擊離開主畫面(我的日記)
-        exit_diary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intent_back=new Intent(MainActivity.this,diary_cover.class);
-                startActivity(intent_back);
-            }
-        });
-
+        list.setOnItemClickListener(this);
     }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG,"MainActivity.onStart");
-    }
+    public void prc_btnadd(View v)
+    {
+        //按下新增時，要呼叫新增的Activity，利用Intent物件。
+        Intent it = new Intent(this,diary_add.class);
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_TAG,"MainActivity.onStop");
-    }
+        startActivity(it);
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(LOG_TAG,"MainActivity.onDestroy");
+        //關閉本身的Activity。
+        db.close();
+        this.finish();
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(LOG_TAG,"MainActivity.onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(LOG_TAG,"MainActivity.onResume");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(LOG_TAG,"MainActivity.onRestart");
-    }
-
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
@@ -131,12 +75,14 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
         TextView txtid = (TextView) view.findViewById(android.R.id.text1);
 
         //按下Item時，要呼叫編輯的Activity，利用Intent物件帶參數過去。
-        Intent it = new Intent(this,dairy_edit.class);
+        Intent it = new Intent(this,diary_edit.class);
+
         it.putExtra("pid",txtid.getText().toString());
+
         startActivity(it);
 
         //關閉本身的Activity。
+        db.close();
         this.finish();
-
     }
 }
--- app/src/main/java/com/example/diary/diary_edit.java
+++ app/src/main/java/com/example/diary/diary_edit.java
@@ -2,82 +2,103 @@ package com.example.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class diary_edit extends AppCompatActivity {
-    //定義生命週期Tag標籤
-    private static final String LOG_TAG_e="diary_editLifeCycle";
-    private EditText diary_content,diary_topic;
-    private TextView result_date;
+
+    //宣告物件
+    private SQLiteDatabase db = null;
+    private EditText txt_topic;
+    private EditText txt_date;
+    private EditText txt_content;
     private TextView lbl_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(LOG_TAG_e,"diary_cover.onCreate");
         setContentView(R.layout.activity_diary_edit);
-        int int_id = 0;
-        String str_topic = "";
-        String str_data = "";
-        String str_content = "";
-        result_date=(TextView) findViewById(R.id.result_date);
-        diary_content=(EditText) findViewById(R.id.diary_content);
-        diary_topic=(EditText) findViewById(R.id.diary_topic);
-        lbl_id=(TextView) findViewById(R.id.lbl_id);
 
+        txt_topic = (EditText)findViewById(R.id.ed_topic);
+        txt_date = (EditText)findViewById(R.id.ed_date);
+        txt_content = (EditText)findViewById(R.id.ed_content);
+        lbl_id = (TextView)findViewById(R.id.lblid);
+
+        //建立SQLite 資料庫的實體檔案
+        db = openOrCreateDatabase("data.db", Context.MODE_PRIVATE,null);
+
+        //建立資料表的SQL語法
+        String strcreatedb = "CREATE TABLE IF NOT EXISTS " +
+                "mytable (_id INTEGER PRIMARY KEY, topic TEXT, date TEXT, content TEXT) ";
+
+        //執行SQL語法，建立資料表
+        db.execSQL(strcreatedb);
+
+        //再取得編輯的資料。
         Intent it2 = getIntent();
         String strcustid = it2.getStringExtra("pid");
 
-        Cursor cursor = db.rawQuery("SELECT * FROM myTable where _id=" + strcustid ,null);
+        //查詢資料
+        Cursor cursor = db.rawQuery("SELECT * FROM mytable where _id=" + strcustid ,null);
 
+        int int_id = 0;
+        String strtopic = "";
+        String strdate = "";
+        String strcontent = "";
+
+        //如果查詢有資料的話，就跟ListView做連結
         if (cursor != null && cursor.getCount() >=0)
         {
+
             while (cursor.moveToNext()) {
                 int_id = cursor.getInt(0);
-                str_topic = cursor.getString(1);
-                str_data = cursor.getString(2);
-                str_content = cursor.getString(3);
+                strtopic = cursor.getString(1);
+                strdate = cursor.getString(2);
+                strcontent = cursor.getString(3);
             }
         }
-
-        diary_topic.setText(str_topic);
-        result_date.setText(str_data);
-        diary_content.setText(str_content);
+        txt_topic.setText(strtopic);
+        txt_date.setText(strdate);
+        txt_content.setText(strcontent);
         lbl_id.setText(String.valueOf(int_id));
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    public void prc_btnupdate(View v)
+    {
+
+        String strupdatesql = "UPDATE mytable SET " +
+                "topic='" + txt_topic.getText().toString() + "', " +
+                "date='" + txt_date.getText().toString() + "', " +
+                "content='" + txt_content.getText().toString() + "' " +
+                "WHERE _id=" + lbl_id.getText();
+
+        db.execSQL(strupdatesql);
+
+        //完成新增後，要呼叫列表的Activity，利用Intent物件。
+        Intent it = new Intent(this,MainActivity.class);
+
+        startActivity(it);
+
+        //關閉本身的Activity。
+        db.close();
+        this.finish();
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+    public void prc_cancel(View v)
+    {
+        //按下「取消」，就呼叫列表的Activity，利用Intent物件。
+        Intent it = new Intent(this,MainActivity.class);
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+        startActivity(it);
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
+        //關閉本身的Activity。
+        db.close();
+        this.finish();
     }
 }
